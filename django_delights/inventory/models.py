1247298472eaeab315dc09b6c758f5a69e41234b
Latteholic <cafelatte.here@gmail.com>
2022-05-22 23:53:31 +0900

    editing models in model.py

--- django_delights/inventory/models.py
+++ django_delights/inventory/models.py
@@ -1,29 +1,32 @@
 from django.db import models
 
+UNIT_CHOICES = [
+  ("g", "gram"),
+  ("tbsp", "tablespoon"),
+  ("tsp", "teaspoon"),
+  ("l", "liter"),
+  ("cup", "cup"),
+  ("", "")
+]
+
 # Create your models here.
 class Ingredient(models.Model):
-  UNIT_CHOICES = [
-    ("g", "gram"),
-    ("tbsp", "tablespoon"),
-    ("tsp", "teaspoon"),
-    ("l", "liter"),
-    ("cup", "cup"),
-    ("", "")
-  ]
   name = models.CharField(max_length=30, unique=True)
   quantity = models.DecimalField(max_digits=7, decimal_places=1)
   unit_price = models.DecimalField(max_digits=7, decimal_places=2)
   unit = models.CharField(max_length=10, choices=UNIT_CHOICES, default="")
 
-class RecipeRequirement(models.Model):
-  name = models.CharField(max_length=40, unique=True)
-  ingredient = models.ManyToManyField(Ingredient)
-
+# represents an item on the restaurant’s menu
 class MenuItem(models.Model):
-  name = models.CharField(max_length=50, unique=True)
+  title = models.CharField(max_length=50, unique=True)
   price = models.DecimalField(max_digits=6, decimal_places=2)
-  recipe = models.ForeignKey(RecipeRequirement, on_delete=models.CASCADE)
+
+# represents a single ingredient and how much of it is required for an item off the menu
+class RecipeRequirement(models.Model):
+  menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+  ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+  quantity = models.CharField(max_length=10, choices=UNIT_CHOICES, default="")
 
 class Purchase(models.Model):
-  timestamp = models.DateField(auto_now_add=True)
+  timestamp = models.DateTimeField(auto_now_add=True)
   menu_item = models.ForeignKey(MenuItem, on_delete=models.PROTECT)
