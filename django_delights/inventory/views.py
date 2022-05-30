0c2b0f824a306b3a93c09d619743030b4cec5e13
Latteholic <cafelatte.here@gmail.com>
2022-05-31 01:34:09 +0900

    added ListView for Ingredients
    - added ListView
    - set up urls.py for inventory app and connected it with urls.py in the project

--- django_delights/inventory/views.py
+++ django_delights/inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from inventory.models import Ingredient
 
 # Create your views here.
+class IngredientList(ListView):
+  ingredients = Ingredient.objects.all()
+  context = {"ingredients": ingredients}
+  model = Ingredient
+  template_name = "inventory/ingredient_list.html"
